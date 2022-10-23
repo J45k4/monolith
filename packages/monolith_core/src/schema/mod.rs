8b3dba218c029ce2e94989209b776f81a851974c
Jaska <pusanteemu@gmail.com>
2022-10-23 20:56:15 +0300

    Add some types to shema module

--- packages/monolith_core/src/schema/mod.rs
+++ packages/monolith_core/src/schema/mod.rs
@@ -1,13 +1,26 @@
 
+pub struct Decorator {
+    pub name: String,
+    pub args: Vec<DecoratorArg>,
+}
+
 pub struct Field {
-    pub name: String
+    pub name: String,
+    pub nullable: bool,
+    pub opt: bool,
+    pub decorators: Vec<Decorator>,
 }
 
 pub struct Relation {
-
+    pub can_read: bool,
+    pub can_write: bool,
+    pub can_delete: bool,
+    pub target: String,
 }
 
 pub struct Model {
+    pub name: String,
     pub fields: Vec<ModelField>,
-    pub relations: Vec<Relation>
+    pub relations: Vec<Relation>,
+    pub decorators: Vec<Decorator>,
 }
